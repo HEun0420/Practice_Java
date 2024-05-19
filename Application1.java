24d8e1b27b78ad183ba7f5f3f4e18d4810980ba1
배하은 <usialeta@gmail.com>
2024-05-19 22:38:34 +0900

    Add files via upload

---
+++ Application1.java
@@ -0,0 +1,13 @@
+package com.ohgriraffers.section01.arithmetic;
+
+public static void main(String[] args) {
+    //* 자바의 정석 02 변수
+
+    /* 정수형
+    byte bnum; // 1 byte
+    short snum; // 2 byte
+    int inum; // 4 byte
+    long lnum; // 8 byte
+
+
+}
