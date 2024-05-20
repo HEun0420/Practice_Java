38726fc8a4920807573e4295a8084272310dfe3b
HEun0420 <usialeta@gmail.com>
2024-05-20 16:19:38 +0900

    0

---
+++ src/main/java/ohgiraffers/example.java
@@ -0,0 +1,11 @@
+package ohgiraffers;
+
+public class example {
+    public static void main(String[] args) {
+
+        System.out.print("Hello World!");
+        System.out.println("졸리당...다음에는 좀 더 일찍 자야지");
+
+    }
+}
+
